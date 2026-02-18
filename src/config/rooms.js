4ceb360244a341d139c1886dc038a7a147ef780d
dvergel-hash <dvergel@fyo.com>
2026-02-18 16:07:53 -0300

    Update rooms.js

--- src/config/rooms.js
+++ src/config/rooms.js
@@ -8,26 +8,32 @@ export const rooms = [
     id: 2,
     name: 'Sala 8 Webinar',
     url: 'https://outlook.office365.com/owa/calendar/8ccab5f71c89461c81084670f1a23e84@fyo.com/12bd5c5aa54840a0b81e468cdc1f99a94192387956609431661/calendar.html' 
+  },
 {
     id: 3,
     name: 'Sala 7 Piso 2',
     url: 'https://outlook.office365.com/owa/calendar/f9cd1bde1e6b4ee28d8ccc9790d198b1@fyo.com/736ca90c921e49d2884ad57b3cf201c72512950827752770642/calendar.html' 
+  },
 {
     id: 4,
     name: 'Sala 6 Piso 2',
     url: 'https://outlook.office365.com/owa/calendar/b3ae13e177e647fd882e7a42722da58c@fyo.com/fa966554b3774ffa905a605a68dc98b81024692252300818748/calendar.html' 
+  },
 {
     id: 5,
     name: 'Sala 5 Piso 2',
     url: 'https://outlook.office365.com/owa/calendar/a0745d67ebfd497ba9ef50de90a6503c@fyo.com/23a3ecbe98b94b6ba49f9a4356894f9f10151906529865444446/calendar.html' 
+  },
 {
     id: 6,
     name: 'Sala 4 - FYO - 1er Piso',
     url: 'https://outlook.office365.com/owa/calendar/cbc6025e6dce4657adf8da3c8cf8d4d2@fyo.com/cf21c1af664b4a179a6c53beaa0beb7114977614626489232636/calendar.html' 
+  },
 {
     id: 7,
     name: 'Sala 3 - FYO - 1er Piso',
     url: 'https://outlook.office365.com/owa/calendar/013c837e34f042a9b7155baa0a1dd113@fyo.com/20049c021d654fe898b85ef7c873275c14938303764017615974/calendar.html' 
+  },
 {
     id: 8,
     name: 'Sala 2 - FYO - 1er Piso',
@@ -37,20 +43,15 @@ export const rooms = [
     id: 9,
     name: 'Sala 1 - FYO - 1er Piso',
     url: 'https://outlook.office365.com/owa/calendar/ecad9b3457d94255a3ffec2f9764ca72@fyo.com/70aa398ef4fa4d329a3ea9aab124b7874729919633465413074/calendar.html' 
-  }
   },
 {
     id: 10,
     name: 'Sala Capacitaciones FYO A - PB',
     url: 'https://outlook.office365.com/owa/calendar/721d3388bf264c96ae4fe690185c2051@fyo.com/0009224775e8477185b2f889e8a24a9212086885284830913460/calendar.html' 
-  }
   },
 {
     id: 11,
     name: 'Sala Capacitaciones FYO B - PB',
     url: 'https://outlook.office365.com/owa/calendar/17869d0f6152465b99f31bcec1ad7766@fyo.com/719e46a60b484ed584e648a81744cb906175246473441368608/calendar.html' 
-  }
-
-
   },
 ];
